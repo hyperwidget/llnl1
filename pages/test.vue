37bed7ba7a5633475908d1a2e782f4f5e9f50191
Hunter Jansen <rawkamatic@gmail.com>
2020-03-29 13:08:49 -0400

    initial commit

---
+++ pages/test.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="section">
+    <div class="columns is-mobile">
+      <card title="Free" icon="github-circle">
+        HI
+        <a href="https://github.com/buefy/buefy">
+          GitHub
+        </a>
+      </card>
+
+      <card title="Lightweight" icon="arrange-bring-to-front">
+        No other internal dependency
+      </card>
+    </div>
+  </section>
+</template>
+
+<script>
+import Card from '~/components/Card'
+
+export default {
+  name: 'HomePage',
+
+  components: {
+    Card
+  }
+}
+</script>
